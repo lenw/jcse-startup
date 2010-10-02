7728bf286d606e982c5326f2177fa824f236ac68
= <robert@icomp.co.za>
2010-10-02 19:11:25 +0200

    Re-commit

---
+++ lis/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listy::Application.config.secret_token = 'a8ff4d70230af5c3c6585eaf3b62876ae6f5c5cf967fe34600cf70ac0c4d61ec64716317a0e78da95cb5b46bbece93a828518f515029a7a619b7350e0566d717'
