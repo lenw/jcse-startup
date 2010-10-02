b156648ff3142cab40b06514972af87d4b9048c9
= <robert@icomp.co.za>
2010-10-02 18:35:45 +0200

    Authentication Done

---
+++ list/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+List::Application.config.secret_token = 'b65b1b2a13c289b41e4ec9cfb88e258397f36e640ccd1e0e21df39fd61b5e1ad27312b36544e2e59fbccfaaeac6b27e4e2a14587936399d5693a3cbc3e64e86c'
