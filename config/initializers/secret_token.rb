89a3661350e1b8e2e6135ba82a12cec0dee5ed8e
= <robert@icomp.co.za>
2010-10-02 12:43:32 +0200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JcseStartup::Application.config.secret_token = '1f53f8936bbf6496d97c972a86be8e5c5fdf3516bc280deee23c34871c4866c221c3818516572f707dc005efaaeb1c26093d6e3f83f589906f08c7e5a0beb329'
